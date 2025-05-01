800c0b1cc595f237f8b9cc3081b3b6a7b946093a
navnet2805 <naveen.rp.2805@gmail.com>
2025-05-01 12:49:03 +0530

    Create Program_04.java

---
+++ src/InterviewQuestions/Selenium/Program_04.java
@@ -0,0 +1,11 @@
+package InterviewQuestions.Selenium;
+/*
+write a relative xpath for 'student' radio button in Makemytrip.
+ */
+public class Program_04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Rahul");
+
+    }
+}
